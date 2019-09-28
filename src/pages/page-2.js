c2680ff5d03e888e5b1d951dcfcdd083bef672c3
gjcarrow <gjcarrow@gmail.com>
2019-09-28 01:17:50 -0400

    initial commit

--- src/pages/page-2.js
+++ src/pages/page-2.js
@@ -7,9 +7,9 @@ import SEO from "../components/seo"
 const SecondPage = () => (
   <Layout>
     <SEO title="Page two" />
-    <h1>Hi from the second page</h1>
+    <h1>Hi from the retarded seconds page</h1>
     <p>Welcome to page 2</p>
-    <Link to="/">Go back to the homepage</Link>
+    <Link to="/">Go back to the where you came from</Link>
   </Layout>
 )
 
